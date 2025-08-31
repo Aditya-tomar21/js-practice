a006959b2d370eb5ab66f097190207261217ff1d
Aditya Singh Tomar <114979998+Aditya-tomar21@users.noreply.github.com>
2025-08-31 13:38:52 +0000

    lec4-varibales / const , let , var

---
+++ lec1_basics/var.js
@@ -0,0 +1,17 @@
+const accountId = 114553
+let accountEmail = "aditya@gmail.com"
+var accountPassword = "123456"
+accountCity = "jaipur"
+let accountName;
+
+// accountId=2  //not allowed hai const to change krna
+
+accountEmail="mehoondon@gmailo.com"
+accountPassword="345"
+accountCity = "agra"
+ console.log(accountId);
+// console.log(accountPassword);
+// console.log(accountCity);
+// console.log(accountEmail); //table k form me sabko sath me leleneg
+
+console.table([accountEmail,accountId,accountPassword,accountCity , accountName]); // yeh change ho jayega
