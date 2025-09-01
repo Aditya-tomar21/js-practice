1c87899574ee6c2b365480c1b32b38a716cb39f5
Aditya Singh Tomar <114979998+Aditya-tomar21@users.noreply.github.com>
2025-09-01 13:04:58 +0000

    datatypes basics

--- lec1_basics/datatype.js
+++ lec1_basics/datatype.js
@@ -1,1 +1,23 @@
-console.log("hello ji");
+//  "use strict"; // treat all js code as newer version but aajkal default yehi hai
+
+//  alert(hello) // we are using node js not browser
+
+console.log(3+
+    3)
+    ; // correct but not good way to write , code readability should be high
+
+
+console.log("aditya");
+
+//datatypes//
+let name = "aditya";  //string
+let age = 24;          //num
+let isloggedIn = false    //boolean
+//null => standalone value i.e. empty value
+//undefined => not defined 
+//symbol => to find uniquene
+
+//baadme padenge object
+
+console.log(typeof "aditya");
+// null is object types , undefined is undefined types check kr skte ho
