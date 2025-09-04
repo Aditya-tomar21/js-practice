cb579dde2c245e6e1846dce31d7dd34d3245f895
Aditya Singh Tomar <114979998+Aditya-tomar21@users.noreply.github.com>
2025-09-04 16:35:33 +0000

    comparison

---
+++ lec1_basics/compare.js
@@ -0,0 +1,35 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+/*******************    always make sure same data types get compared    ********************/
+
+
+
+//console.log("2">1); 
+
+/**************         automatically string 2 get coverted by js for comaparison       *************/
+
+
+
+
+
+
+// console.log(null>0);  false dega
+// console.log(null== 0);   false dega
+// console.log(null>=0);     true dega 
+
+/***************************comparison operator like <, > , >= ,<= changes null to 0 , but == doesnt change so its better to use same data types to comparer *****************************/
+
+
+
+
+
+
+/************      strict check i.e. === , data types and value comapre krega     ******************/
+
+// console.log("2"==2);  true dega
+// console.log("2"===2);  false dega kyuki strict check hota hai
+
