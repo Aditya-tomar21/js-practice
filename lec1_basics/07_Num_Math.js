32fd34c6ff09ac4dc536289a7a05ebeae9c17578
Aditya Singh Tomar <114979998+Aditya-tomar21@users.noreply.github.com>
2025-09-05 13:59:52 +0000

    maths and num

---
+++ lec1_basics/07_Num_Math.js
@@ -0,0 +1,33 @@
+// const score = 400 
+// console.log(score)
+
+// const balance = new Number (100)
+// console.log(balance);
+
+// console.log(balance.toFixed(2)); // upto 2 decimal places
+
+// const otherNumber = 123.3343
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+/*********************************           MATHS      *********************************/
+
+// console.log(Math); //EK OBJECT JISKE METHODS HOTE HAI BHOT SAARE
+
+// console.log(Math.abs(-44));   // absolute- negative to positive
+// console.log(Math.round(4.7));  //round off  , isme floor and ceil bhi hota hai
+
+// console.log(Math.min(2,4,1,5,12,4,1));
+
+console.log(Math.floor(Math.random() * 10) +1);
+
+const min = 10
+const max = 50
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min);  //formula hai yaad krlena
+
+
+
+
