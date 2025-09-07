3f3aed5c7709fbe1f43617ae3020bbd85221e0ba
Aditya Singh Tomar <114979998+Aditya-tomar21@users.noreply.github.com>
2025-09-07 12:09:00 +0000

    array

---
+++ JS_basics02/01_array.js
@@ -0,0 +1,20 @@
+/*********************                        ARRAY                         *****************************/
+
+const MyArr = [3,6,8,11,42] //mix data types could be used
+console.log(MyArr);
+//array make shallow copy ie have same refernce point yaani copy me change og me bhi change layega
+
+
+const myHero = ["goku" , "luffy"]
+
+const MyArr2 = new Array( 3, 45 , 6 )
+
+
+
+/*******************                     ARRAY  Methods           ******************** */
+
+MyArr.push(69)
+console.log(MyArr);
+
+MyArr.pop()
+console.log(MyArr);
