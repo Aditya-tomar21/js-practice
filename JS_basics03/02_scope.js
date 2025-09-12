098c201772e026d8e3189c3a340e96cc6196ef5d
Aditya Singh Tomar <114979998+Aditya-tomar21@users.noreply.github.com>
2025-09-12 13:55:25 +0000

    more in fucntion

--- JS_basics03/02_scope.js
+++ JS_basics03/02_scope.js
@@ -5,12 +5,71 @@ if (true) {
     const b = 22
    // var c = 33  // yeh if k bahar bhi access hoga isliye not good 
 
-   console.log(`inside block value of a is ${a}`);
+   //console.log(`inside block value of a is ${a}`);
    
     
 }
- console.log("value of a global is " ,a);
+ //console.log("value of a global is " ,a);
 // console.log(b);
 //console.log(c);
 
 
+
+///*******            parent - child , child can access parents but vice versa not true         */
+
+
+function one() 
+{
+    const username = "Hadityaa"
+
+    function  two()
+     {
+         const website = "youtube"
+         //console.log(username);
+     }
+
+//console.log(website);
+two()
+}
+
+one()
+
+if (true)
+{
+    const username = "hulkbhai"
+    if(username === "hulkbhai")
+    {
+        const website = " twitter"
+        //console.log(username + website);
+        
+    }
+    //console.log(website);
+    
+}
+
+//console.log(username);
+
+
+
+
+
+/*****************                            INTERESTING                    ******************* */
+
+
+//two way of function
+
+
+//  addone(5)     yeh kaam krega
+function addone(Num){
+    return Num +1
+}
+addone(5);
+
+
+
+//addtwo(5)   yeh idar nhi kaam krega
+const addtwo = function(num){
+    return num+2
+}
+
+addtwo(5)
