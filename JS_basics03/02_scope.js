50a4a76d7ba39c911f0ea85ee216360cfaa25583
Aditya Singh Tomar <114979998+Aditya-tomar21@users.noreply.github.com>
2025-09-12 13:25:35 +0000

    scope in js

---
+++ JS_basics03/02_scope.js
@@ -0,0 +1,16 @@
+//var c = 300
+let a = 112
+if (true) {
+    let a = 11
+    const b = 22
+   // var c = 33  // yeh if k bahar bhi access hoga isliye not good 
+
+   console.log(`inside block value of a is ${a}`);
+   
+    
+}
+ console.log("value of a global is " ,a);
+// console.log(b);
+//console.log(c);
+
+
