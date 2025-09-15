8000f680e149d32999e142d07c21a7072a4dac27
Aditya Singh Tomar <114979998+Aditya-tomar21@users.noreply.github.com>
2025-09-15 18:04:02 +0000

    switch

---
+++ JS_ControlFlow04/02_switch.js
@@ -0,0 +1,52 @@
+/**********************         switch case syntax        ******************* 
+ 
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
+
+
+*/
+
+
+const month = 3
+//copy paste -> select -> shift + alt + downarrow
+switch (month) {
+    case 1:
+        console.log("jan");
+        
+        break;
+
+    case 2:
+        console.log("feb");
+        
+        break;
+
+    case 3:
+        console.log("mar");
+        
+        break;
+
+    case 4:
+        console.log("apr");
+        
+        break;
+
+    case 5:
+        console.log("may");
+        
+        break;
+
+
+    default:
+        console.log("no month match");
+        
+        break;
+}
+// break rok deta hai execution nhi to true k baad sab cases chal jayenge
