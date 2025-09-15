2f3e24741e447768161cda1964620e98c6e73a9c
Aditya Singh Tomar <114979998+Aditya-tomar21@users.noreply.github.com>
2025-09-15 17:48:35 +0000

    if else

---
+++ JS_ControlFlow04/01_CF.js
@@ -0,0 +1,55 @@
+/****************            if statement             ************** */
+
+// const IsUserLoggedIn = true
+
+// if(condition) //if true toh ander jayega , else nhi jayega
+// {
+
+// }
+
+// else { // if k false hone pr yeh execute hoga hi hoga
+
+// }
+
+//comparison operator      >, < , <= , => , ==  , != , ===(also compare data type) , !==
+
+const score = 200
+if(score>100)
+{
+    const power = "fly"
+    console.log(`superpower is to ${power}`); 
+}
+
+console.log(power);  //isme power not reachable due to scope , but var power se ho jata kyuki uska scope global hota hai
+
+
+const balance = 1000
+if(balance>500)console.log("test"),console.log("test2");
+ //single line me likho , and , comma lagakr bhi mutliple lines hota hai
+
+
+ /*****            if - else if  - else       ************/
+ if(cond1)
+ { }
+
+ else if(cond2)
+ {  }
+
+ else if(cond3) 
+ {  }
+
+else
+{  }
+ 
+
+
+
+if(condition1  &&  condition2)   // dono true hone hai && se
+{   }
+
+
+if(conditon1 || condition2)    // any one true ho || or value se
+{    }
+
+
+
