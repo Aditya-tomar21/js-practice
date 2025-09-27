58d35291d935cba1119cd120acd61d18694cc098
Aditya Singh Tomar <114979998+Aditya-tomar21@users.noreply.github.com>
2025-09-27 12:59:11 +0000

    while - dowhile

---
+++ JS_iterations05/two.js
@@ -0,0 +1,24 @@
+// let i = 0
+// while(i<=5 )
+// {
+// console.log(`value of i is ${i}`);
+// i+=1
+// }
+
+
+
+// let myArray = ["goku" , " vegeta" , "broly"]
+// let arr = 0
+// while(arr<myArray.length)
+// {
+// console.log(`value is ${myArray[arr]}`);
+// arr = arr+1
+
+// }
+
+let score = 12
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+} while (score<=10);
