a715a35575f47db66b2922a2d6e03fa0b938d7d7
Aditya Singh Tomar <114979998+Aditya-tomar21@users.noreply.github.com>
2025-09-27 14:44:08 +0000

    filter and for each

---
+++ JS_iterations05/06.js
@@ -0,0 +1,82 @@
+//  ***********************       filter and for-each
+
+
+// const coding = ["js","py","cpp","ruby"]
+
+// const values = coding.forEach( (item) =>{
+//     //console.log(item);
+//     return item;   // nhi hoga return chahe likho return ya nhi
+// } )
+// console.log(values);
+
+
+
+
+
+
+//************* using           filter         that returns some value if needed  */
+
+ const myNums = [1,2,3,4,5,6,7,8,9,10]
+// // const newNums = myNums.filter(   (num) => (num>5)  )   // return bina likhe hota hai ()=>()  se
+
+// const newNums = myNums.filter(   (num) => {
+//     // num>4   return krna padega
+//     return num>5
+// }  )
+
+// console.log(newNums);
+
+
+
+// ********************         taking value using for each using logic
+
+// const newNums =[]
+// myNums.forEach( (num) => {
+//     if(num>4)
+//     {
+//         newNums.push(num)
+//     }
+// } )
+
+// console.log(newNums);
+
+
+
+//    using filter in array of objects   for real life example
+const books = [
+  {
+    bookName: "The Alchemist",
+    publish: 1988,
+    version: "1",
+    genre: "Fiction"
+  },
+  {
+    bookName: "Clean Code",
+    publish: 2008,
+    version: "2",
+    genre: "Programming"
+  },
+  {
+    bookName: "Atomic Habits",
+    publish: 2018,
+    version: "1",
+    genre: "Self-help"
+  },
+  {
+    bookName: "Harry Potter and the Sorcerer's Stone",
+    publish: 1997,
+    version: "2",
+    genre: "Fantasy"
+  },
+  {
+    bookName: "The Pragmatic Programmer",
+    publish: 1999,
+    version: "2",
+    genre: "Programming"
+  }
+]
+
+const plish = books.filter( (bk) => (bk.version==2))
+console.log(plish);
+
+
