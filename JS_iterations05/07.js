04bf203f5a1cf72fd957997005ed4de5439c79a5
Aditya Singh Tomar <114979998+Aditya-tomar21@users.noreply.github.com>
2025-09-27 15:51:24 +0000

    .map and .reduce

---
+++ JS_iterations05/07.js
@@ -0,0 +1,71 @@
+// /****************             .MAPS           */
+
+
+// const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// // const newNums = myNums.map( (num)=>(num + 10 ) )
+
+
+// //****************************             chaining of map , map 
+// const newNums = myNums
+//                       .map( (num) => num*10 )
+//                       .map( (num) => num+1 )   // isme nums ki chnaged ho chuki value ayegi ie num*10
+//                       .filter( (num) => num>=40)
+// console.log(newNums);
+
+
+
+
+/*                            Reduce                                          */
+
+
+const myNums = [1,2,3]
+
+//using normal function
+
+// const myTotal = myNums.reduce( function (acc ,currVal)
+//  {
+//     console.log(`acc:${acc} and currVal:${currVal}`);
+//     return acc+currVal
+
+// }, 3)
+
+
+//using arrow function
+// const myTotal = myNums.reduce( (acc,curr) => (acc+curr),0)
+// console.log(myTotal);
+
+
+const shopCart = 
+[
+    {itemName : "java course",
+        price : 999
+    },
+
+    {itemName : "python course",
+        price : 2999
+    },
+
+
+    {itemName : "cpp course",
+        price : 599
+    },
+
+
+    {itemName : "AI course",
+        price : 799
+    },
+
+
+    {itemName : "webdev course",
+        price : 999
+    },
+
+
+    {itemName : "appdev course",
+        price : 4999
+    }
+]
+
+const priceToPay = shopCart.reduce( (acc,item)=> (acc + item.price), 0)
+console.log(priceToPay);
