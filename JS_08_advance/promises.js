b77a0698811ad60261da76ca1a69ead5f43f02a4
Aditya Singh Tomar <114979998+Aditya-tomar21@users.noreply.github.com>
2025-10-14 17:15:57 +0000

    fetch() , async await

--- JS_08_advance/promises.js
+++ JS_08_advance/promises.js
@@ -1,81 +1,83 @@
-// //promise example 1
+//promise example 1
 
 
-// const promiseOne = new Promise((resolve,reject)=>{
-//     //do async task
-//     setTimeout(function(){
-//         console.log('asyn task is compelete');
-//         resolve()
+const promiseOne = new Promise((resolve,reject)=>{
+    //do async task
+    setTimeout(function(){
+        console.log('asyn task is compelete');
+        resolve()
         
-//     }, 1000 )
-// })
-// promiseOne.then(function(){
-//     console.log('promise consumed');
+    }, 1000 )
+})
+promiseOne.then(function(){
+    console.log('promise consumed');
     
-// })
+})
 
 
 
 
 
-// //promise example 2
+//promise example 2
 
-// new Promise(function(resolve,reject){
-//     setTimeout(function() {
-//         console.log("aysn task 2")
-//         resolve()
-//     }, 1000);   
+new Promise(function(resolve,reject){
+    setTimeout(function() {
+        console.log("aysn task 2")
+        resolve()
+    }, 1000);   
 
-// }).then(function(){
-//     console.log('task2 chal chuka hai');
+}).then(function(){
+    console.log('task2 chal chuka hai');
     
-// })
+})
+
+
+// resolve  jo hai woh return krta hai value  , joki then k callback fucntion me aati hai
 
 
 
+// promise example 3
 
+const promiseThree = new Promise(function(resolve,reject){
+    setTimeout(function(){
 
+        resolve({username:"chai", email:"abcd@gamil.com"})
+    }, 1000)
+})
 
-//promise example 3
-
-// const promiseThree = new Promise(function(resolve,reject){
-//     setTimeout(function(){
-//         resolve({username:"chai", email:"abcd@gamil.com"})
-//     }, 1000)
-// })
-// promiseThree.then(function(user){
-//     console.log(user);
+promiseThree.then(function(user){
+    console.log(user);
     
-// })
+})
 
 
 
 
-//promise 4
+// promise 4
 
-// const promiseFour = new Promise(function(resolve,reject){
-//     setTimeout(function(){
-//         let error = false;
-//         if(!error)
-//         {
-//             resolve({username:"aditya" , password:"123"})
-//         }
-//         else{
-//             reject('ERROR:something went wrong')
-//         }
-//     } , 1000)
-// })
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = false;
+        if(!error)
+        {
+            resolve({username:"aditya" , password:"123"})
+        }
+        else{
+            reject('ERROR:something went wrong')
+        }
+    } , 1000)
+})
 
-// promiseFour.then((user)=>{
-//     console.log(user);
-//     return(user.username)
-// }).then(function(Myusername){                                //isme chainng hoti hai 
-//     console.log(Myusername);
+promiseFour.then((user)=>{
+    console.log(user);
+    return(user.username)                                   // iska return next then me jayega as value 
+}).then(function(Myusername){                                //isme chainng hoti hai 
+    console.log(Myusername);
     
-// }).catch(function(error){
-//     console.log(error);
+}).catch(function(error){
+    console.log(error);
     
-// }).finally(()=>console.log("promise is resolved or rejected"))
+}).finally(()=>console.log("promise is resolved or rejected"))
 
 
 
@@ -83,21 +85,64 @@
 
 const promiseFive = new Promise(function(resolve,reject){
       setTimeout(function(){
-        let error = false;
+        let error = true;
         if(!error)
         {
             resolve({username:"champaaa" , password:"996969"})
         }
         else{
-            reject('ERROR:something went gadbad !!!!!!!!')
+            reject('ERROR: promsie5 something went gadbad !!!!!!!!')
         }
     } , 1000)
 })
 
+//async similar hota hai then catch k , yeh thoda time leta hai execute hone me 
+//async - await use hota hai
+
 async function consumePromiseFive(){
 
-    const response = await promiseFive;
-    console.log(response);
+    try {
+        const response = await promiseFive;   //wait kr rha hai await jo bhi as response ayega from promisefive
+    console.log(response); 
 
+    } 
+
+    catch (error) {
+        console.log(error);  
+    }
 }
 consumePromiseFive();
+
+
+
+// async function getAllUser()
+// {
+//     try
+//     {
+//         const response = await  fetch('https://api.github.com/users/aditya-tomar21')
+//         // console.log(response);
+        
+//         const data = await response.json()
+//         console.log(data);
+  
+//     } 
+    
+//     catch (error)
+//     {
+//         console.log("E:", error);
+      
+//     }
+
+// }
+// getAllUser()
+
+
+//getting directly using fetch()
+fetch('https://api.github.com/users/aditya-tomar21')     // promise return krega yaani reposne ka wait krna padega
+.then((response)=>{
+    return response.json()
+}).then((data)=>{
+    console.log(data);
+    
+})
+.catch((error)=>console.log(error))
