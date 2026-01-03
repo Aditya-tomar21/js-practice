0c3b167c5eba9c28cf732b3eb347dee7f86e07f2
Aditya Singh Tomar <114979998+Aditya-tomar21@users.noreply.github.com>
2026-01-03 16:35:42 +0000

    classes

--- JS_09_OOPS/myClasses.js
+++ JS_09_OOPS/myClasses.js
@@ -1,3 +1,26 @@
 class User{
-    constructor(usern )
+    constructor(username,email,password ){
+        this.username=username;
+        this.email=email;
+        this.password= password;
+    }
+
+    encryptPassword()
+    {
+        return console.log(`${this.password}abc`);
+        
+    }
+
+    ChangeUserName()
+    {
+        return console.log(`${this.username.toUpperCase()}`);
+        
+    }
 }
+
+const chai= new User("chai","chai@gmail.com","123")
+console.log(chai.encryptPassword());
+console.log(chai.ChangeUserName());
+
+
+//above could also beused using prototype and fucntions
